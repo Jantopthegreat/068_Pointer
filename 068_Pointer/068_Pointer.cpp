9ecf10ef708c9881d691bd89fa900cadeb99d3a7
Jantopthegreat <fauzangriya@gmail.com>
2023-05-10 10:49:05 +0700

    membuat object dan memberi akses kepada member

--- 068_Pointer/068_Pointer.cpp
+++ 068_Pointer/068_Pointer.cpp
@@ -11,4 +11,20 @@ public:
 void mahasiswa::showNim() { //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 };		// object mhs
+	mhs.showNim();			//Member Access Operator
+
+	mahasiswa& ref = mhs;	//Pointer Reference refMhs
+	ref.nim = 1;		//Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		//Pointer dereference pMhs
+	pMhs->nim = 3;				//Arrow operator
+	pMhs->showNim();
+	system("pause");
+
+}
 	
